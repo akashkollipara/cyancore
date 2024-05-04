5ad5a941f490c0f6f43686f531289d43fb9c25ac
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2024-05-04 15:16:05 -0700

    Create eeprom_private.h

---
+++ src/platform/mega_avr/common/hal/eeprom/eeprom_private.h
@@ -0,0 +1,13 @@
+/*
+ * CYANCORE LICENSE
+ * Copyrights (C) 2024, Cyancore Team
+ *
+ * File Name		: eeprom_private.h
+ * Description		: This file contains definitions used by eeprom HAL
+ * Primary Author	: Shinika Balasundar [shinika.balasundar@yahoo.com]
+ * Organisation		: Cyancore Core-Team
+ */
+
+#pragma once
+
+/* Question: Process for ifndef and define and endif? ex: #define _SPI_PRIVATE_H_ */
